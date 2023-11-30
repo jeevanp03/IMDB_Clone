d1734b269e7111ddee0ed0df1066aa8885759b8c
Jeevan Parmar <93451143+jeevanp03@users.noreply.github.com>
2023-11-30 09:51:55 -0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
-  user    : 'j29parma',
-  password: 'Jeevan2003$',
-  database: 'j29parma'
-};
- 
-export default config;
